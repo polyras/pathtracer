f1b775a4d53a9976d2aeb03ac0db8fad9513d925
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2016-06-08 12:50:38 +0200

    Minor code style fix in assert code

--- code/lib/assert.cpp
+++ code/lib/assert.cpp
@@ -1,13 +1,13 @@
 #include "assert.h"
 
-void _DebugAssert(bool Expression, const char *Filename, size_t Line) {
+void _DebugAssert(bool Expression, char const *Filename, size_t Line) {
   if(!Expression) {
     fprintf(stderr, "Assert failed: %s:%zu\n", Filename, Line);
     exit(1);
   }
 }
 
-void ReleaseAssert(bool Expression, const char *Message) {
+void ReleaseAssert(bool Expression, char const *Message) {
   if(!Expression) {
     fprintf(stderr, "Assert failed: %s\n", Message);
     exit(1);
