eab7cbf46e8ee8db6b060b0f1b65ab674a31fd88
Rasmus Rønn Nielsen <rasmusrnielsen@gmail.com>
2016-06-14 10:09:21 +0200

    Removed debug assert from release build

--- code/lib/assert.cpp
+++ code/lib/assert.cpp
@@ -1,11 +1,13 @@
 #include "assert.h"
 
+#ifdef DEBUG
 void _DebugAssert(bool Expression, char const *Filename, size_t Line) {
   if(!Expression) {
     fprintf(stderr, "Assert failed: %s:%zu\n", Filename, Line);
     exit(1);
   }
 }
+#endif
 
 void ReleaseAssert(bool Expression, char const *Message) {
   if(!Expression) {
--- code/lib/assert.h
+++ code/lib/assert.h
@@ -3,7 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#if DEBUG
 void _DebugAssert(bool Expression, const char *Filename, size_t Line);
 #define DebugAssert(Expression) _DebugAssert(Expression, __FILE__, __LINE__)
+#else
+#define DebugAssert(Expression)
+#endif
+
 void ReleaseAssert(bool Expression, const char *Message);
 #define InvalidCodePath DebugAssert(false)
